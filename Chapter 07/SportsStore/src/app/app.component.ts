2077b97cf6d70a469d941d68ca273507fa4c11d3
` <dthuya17007@cusc.ctu.edu.vn>
2019-10-02 15:40:43 +0700

    Update

---
+++ Chapter 07/SportsStore/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:quotemark
+    // tslint:disable-next-line:component-selector
+    selector: 'app',
+    template: '<store></store>'
+})
+export class AppComponent { }
